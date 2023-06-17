666182cbca16eddc6137848c9db2f7060bd6967d
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-16 19:07:04 -0600

    ci: Defined Forecasts layout

---
+++ src/layouts/Forecasts/Forecasts.tsx
@@ -0,0 +1,17 @@
+import './Forecasts.scss'
+import Forecast from '@/components/Forecast/Forecast'
+import { RootState } from '@/store/store'
+import { useSelector } from 'react-redux'
+
+function Forecasts() {
+	const { forecasts } = useSelector((state: RootState) => state.forecast)
+
+	return (
+		<section id="forecasts">
+			{forecasts.map((forecast) => (
+				<Forecast key={forecast.day} data={forecast} />
+			))}
+		</section>
+	)
+}
+export default Forecasts
