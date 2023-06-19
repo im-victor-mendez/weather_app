40b9f5a661f4e8b04cc0d7c70ecec0776345b778
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-06-19 11:31:29 -0600

    fix: Fixed path usage in Current layout

--- src/layouts/Current/Current.tsx
+++ src/layouts/Current/Current.tsx
@@ -49,8 +49,8 @@ function Current() {
 
 	const imageSrc =
 		Object.keys(currentWeather).length > 0
-			? IconIndex[currentWeather.icon_num - 1].replace('@', './src/')
-			: IconIndex[1].replace('@', './src/')
+			? IconIndex[currentWeather.icon_num - 1].replace('@', '..')
+			: IconIndex[1].replace('@', '..')
 	const description = desSlug(currentWeather.icon || '')
 	const date = new Date().toUTCString()
 	const today = translateDate(date)
