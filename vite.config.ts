3242de670ed26f81d436e08aedc5658327fe79f0
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-07-18 13:12:40 -0600

    fix: Partially fixed root alias path use

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 /// <reference types="vite-plugin-svgr/client"/>
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
-	resolve: {
-		alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-	},
+	plugins: [react(), svgr(), tsconfigPaths()],
 })
