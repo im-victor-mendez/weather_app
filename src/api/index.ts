313a83c024a29fa62977009544092616b20b1e9e
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-07-18 10:27:31 -0600

    fix: Fixed fetch option headers into api index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 export const apiUrl = 'https://ai-weather-by-meteosource.p.rapidapi.com/'
-export const apiHeaders = JSON.parse(import.meta.env.VITE_API_HEADERS)
-export const apiOptions = {
+export const apiOptions: RequestInit = {
 	method: 'GET',
-	headers: apiHeaders,
+	headers: {
+		'x-rapidapi-key': import.meta.env.VITE_API_KEY,
+		'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com',
+	},
 }
